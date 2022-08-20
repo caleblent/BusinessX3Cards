81ac7d75ceeb9309fc2e55b496e167ae928c1b69
Caleb Lent <caleb@stcroixwebagency.com>
2022-08-20 17:38:08 -0500

    built out smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: "0.8.11",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+}
